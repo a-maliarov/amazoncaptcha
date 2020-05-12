2137bef02b7e60746adfca5148f6f04782f9d151
Anatolii <tly.mov@gmail.com>
2020-05-12 18:26:08 +0300

    Setups 0.0.9

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="amazoncaptcha",
+    version="0.0.9",
+    description="Solving captchas never ends...",
+    packages=['amazoncaptcha'],
+    py_modules=['objects'],
+    include_package_data = True,
+    package_data = {
+        '': ['*.json'],
+        'amazoncaptcha': ['data/*.*'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires = [
+        "pillow ~= 7.1.2"
+    ],
+    author="Anatolii Maliarov",
+    author_email="tly.mov@gmail.com",
+    url="https://github.com/a-maliarov/Amazon-Captcha-Solver",
+)
