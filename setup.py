e6c843b2ac06e5036bfb099ac430a08f36625ff3
Anatolii <tly.mov@gmail.com>
2020-05-13 15:14:01 +0300

    Update setup to version 0.0.10

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="amazoncaptcha",
-    version="0.0.9",
+    version="0.0.10",
     description="Solving captchas never ends...",
     packages=['amazoncaptcha'],
     py_modules=['objects'],
