c7a42a46826686f68e98a48c2c103fd99f71f0c4
Anatolii <tly.mov@gmail.com>
2020-05-15 17:04:53 +0300

    Add unittest for main

---
+++ tests/test_main.py
@@ -0,0 +1,19 @@
+from amazoncaptcha import AmazonCaptcha
+import unittest
+import os
+
+class TestAmazonCaptcha(unittest.TestCase):
+
+    current_location = os.path.abspath(os.path.dirname(os.path.abspath(__file__))) + os.sep
+
+    def test_solve(self):
+        solutions = list()
+
+        for file in os.listdir(self.current_location + 'captchas' + os.sep):
+            captcha = AmazonCaptcha(self.current_location + 'captchas' + os.sep + file)
+            solutions.append(captcha.solve())
+
+        self.assertEqual(solutions, ['KRJNBY', 'UGXGMM'])
+
+if __name__ == '__main__':
+    unittest.main()
