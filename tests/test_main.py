c2cb855ad63b92a67ebfe6d71410ad5fcd09a44a
Anatolii <tly.mov@gmail.com>
2020-05-21 09:25:14 +0300

    Update unittest to cover version 0.2.3

--- tests/test_main.py
+++ tests/test_main.py
@@ -13,24 +13,16 @@ class TestAmazonCaptcha(unittest.TestCase):
         self.assertEqual(solution, 'UGXGMM')
 
     def test_error(self):
-        solution = AmazonCaptcha('tests/captchas/error.png').solve()
-        self.assertEqual(solution, 'Error')
+        solution = AmazonCaptcha('tests/captchas/corrupted_1.png').solve()
+        self.assertEqual(solution, 'BPXHGH')
 
     def test_error_1(self):
-        solution = AmazonCaptcha('tests/captchas/error_1.png').solve()
-        self.assertEqual(solution, 'Error')
+        solution = AmazonCaptcha('tests/captchas/corrupted_2.png').solve()
+        self.assertEqual(solution, 'KMGMXE')
 
     def test_notsolved(self):
         solution = AmazonCaptcha('tests/captchas/notsolved.jpg').solve()
         self.assertEqual(solution, 'Not solved')
-
-    def test_output_dict(self):
-        solution = AmazonCaptcha('tests/captchas/notcorrupted.jpg', onreturn = 'dict').solve()
-        self.assertEqual(solution, {'1': 'K', '2': 'R', '3': 'J', '4': 'N', '5': 'B', '6': 'Y'})
-
-    def test_output_raw_dict(self):
-        solution = AmazonCaptcha('tests/captchas/notcorrupted.jpg', onreturn = 'raw_dict').solve()
-        self.assertEqual(solution, {'1': 'K', '2': 'R', '3': 'J', '4': 'N', '5': 'B', '6': 'Y'})
 
     def test_from_webdriver(self):
         capabilities = webdriver.ChromeCapabilities()
