d754a511f9e6dbec0016759c49f1254f1f49705b
Anatolii <tly.mov@gmail.com>
2020-05-24 01:53:41 +0300

    Update setup.py to version 0.3.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="amazoncaptcha",
-    version="0.3.8",
+    version="0.3.9",
     description="Pure Python, lightweight, Pillow-based solver for the Amazon's text captcha.",
     packages=['amazoncaptcha'],
     py_modules=['objects'],
