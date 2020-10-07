8a0011e55c8240b43233fd0e0fc5b0525a754299
Anatolii <53064218+a-maliarov@users.noreply.github.com>
2020-10-07 19:42:57 +0300

    Define classifiers beforehand (#20)

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ import setuptools
 
 here = os.path.abspath(os.path.dirname(__file__))
 
+about = dict()
+with open(os.path.join(here, 'amazoncaptcha', '__version__.py'), 'r', encoding='utf-8') as f:
+    exec(f.read(), about)
+
 def readme(logo_end_line=14):
     """Extracts the logo from README file before pushing to PyPi"""
 
@@ -11,9 +15,26 @@ def readme(logo_end_line=14):
 
     return long_description
 
-about = {}
-with open(os.path.join(here, 'amazoncaptcha', '__version__.py'), 'r', encoding='utf-8') as f:
-    exec(f.read(), about)
+classifiers = [
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Development Status :: 5 - Production/Stable",
+    "Natural Language :: English",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Education",
+    "Intended Audience :: Information Technology",
+    "Topic :: Internet :: WWW/HTTP :: Browsers"
+]
+
+requires = [
+    "pillow ~= 7.2.0",
+    "requests ~= 2.24.0",
+    "selenium ~= 3.141.0"
+]
 
 setuptools.setup(
     name=about['__title__'],
@@ -21,32 +42,12 @@ setuptools.setup(
     description=about['__description__'],
     packages=['amazoncaptcha'],
     py_modules=['devtools', 'exceptions', 'solver', 'utils'],
-    include_package_data = True,
-    package_data = {
-        '': ['*.json'],
-        'amazoncaptcha': ['training_data/*.*'],
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Development Status :: 5 - Production/Stable",
-        "Natural Language :: English",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Education",
-        "Intended Audience :: Information Technology",
-        "Topic :: Internet :: WWW/HTTP :: Browsers"
-    ],
+    include_package_data=True,
+    package_data={'': ['*.json'], 'amazoncaptcha': ['training_data/*.*']},
+    classifiers=classifiers,
     long_description=readme(),
     long_description_content_type="text/markdown",
-    install_requires = [
-        "pillow ~= 7.2.0",
-        "requests ~= 2.24.0",
-        "selenium ~= 3.141.0"
-    ],
+    install_requires=requires,
     author=about['__author__'],
     author_email=about['__author_email__'],
     url=about['__url__'],
