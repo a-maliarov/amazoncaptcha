c5e56158c6e3977ae1c567f8833690967566d5fd
Anatolii <53064218+a-maliarov@users.noreply.github.com>
2020-10-11 00:20:08 +0300

    Update the GH version to 0.4.9

--- amazoncaptcha/__version__.py
+++ amazoncaptcha/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'amazoncaptcha'
 __description__ = 'Pure Python, lightweight, Pillow-based solver for Amazon\'s text captcha.'
 __url__ = 'https://github.com/a-maliarov/amazon-captcha-solver'
-__version__ = '0.4.9-dev'
+__version__ = '0.4.9'
 __author__ = 'Anatolii Maliarov'
 __author_email__ = 'tly.mov@gmail.com'
 __license__ = 'MIT'
