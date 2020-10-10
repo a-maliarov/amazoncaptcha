3071a9fd07a838dd3b02631b0f91ff56c96accb2
Anatolii <53064218+a-maliarov@users.noreply.github.com>
2020-10-10 04:08:28 +0300

    Revert the Sphinx version (#12)

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ release = 'v0.4.7'
 # -- General configuration ---------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = "2.4"
+# needs_sphinx = "2.4"
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
