35390be7e0bef80695d4f72438263f21d7ebf391
Anatolii <53064218+a-maliarov@users.noreply.github.com>
2020-10-14 08:00:48 +0300

    Update the version to 0.4.10

--- amazoncaptcha/__version__.py
+++ amazoncaptcha/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'amazoncaptcha'
 __description__ = "Pure Python, lightweight, Pillow-based solver for Amazon's text captcha."
 __url__ = 'https://github.com/a-maliarov/amazoncaptcha'
-__version__ = '0.4.9'
+__version__ = '0.4.10'
 __author__ = 'Anatolii Maliarov'
 __author_email__ = 'tly.mov@gmail.com'
 __license__ = 'MIT'
